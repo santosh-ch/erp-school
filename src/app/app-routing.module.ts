6b840482c84c8e83f4bd93c9f83e14eabdc4ea7d
Santosh ch <cherukurisantosh5@gmail.com>
2021-04-26 09:06:21 +0530

    routing initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdmissionFormComponent } from './admission/admission-form/admission-form.component';
+import { SchoolFaqsComponent } from './admission/school-faqs/school-faqs.component';
+import { HomeComponent } from './home/home.component';
+import { ParentRegistrationComponent } from './parent-corner/parent-registration/parent-registration.component';
+import { RegisterExtraCurricularsComponent } from './parent-corner/register-extra-curriculars/register-extra-curriculars.component';
+import { MarksEntryFormComponent } from './staff/marks-entry-form/marks-entry-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home', children: [
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      {
+        path: 'admission', children: [
+          { path: '', redirectTo: 'admissionform', pathMatch: 'full' },
+          { path: 'admissionform', component: AdmissionFormComponent },
+          { path: 'schoolfaq', component: SchoolFaqsComponent }]
+      },
+      {
+        path: 'parentcorner', children: [
+          { path: '', redirectTo: 'parentreg', pathMatch: 'full' },
+          { path: 'parentreg', component: ParentRegistrationComponent },
+          { path: 'registerec', component: RegisterExtraCurricularsComponent }]
+      },
+      {
+        path: 'staff', children: [
+          { path: '', redirectTo: 'marksentry', pathMatch: 'full' },
+          { path: 'marksentry', component: MarksEntryFormComponent }]
+      }
+    ]
+  },
+  { path: '**', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ParentCornerModule } from './parent-corner/parent-corner.module';
 import { StaffModule } from './staff/staff.module';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
     ],
   imports: [
     BrowserModule,
